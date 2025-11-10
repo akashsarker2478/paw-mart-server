959a671e972ce0a305a5566303262f664d1f900b
Akash Sarker <akashsarker2478@gmail.com>
2025-11-10 17:27:49 +0600

    delete api add

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ async function run() {
       res.send(result)
     })
 
+    //delete product
+    app.delete('/product/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await listingsCollection.deleteOne(query)
+      res.send(result)
+    })
+
     //get api
     app.get('/product',async(req,res)=>{
         const cursor = listingsCollection.find()
